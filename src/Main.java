dbfa0ab6fb595d13d78701371924d48e7f78af83
fausberg <ivanfadeev2003@mail.ru>
2022-09-28 14:40:47 +0300

    Homework_9

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,16 @@ public class Main {
     public static void main(String[] args) {
         Vector vector = new Vector(2);
         Vector vector1 = new Vector(2);
+
         vector.info();
         vector1.info();
+
         vector.vectorPow(vector, vector1);
         vector.info();
+
         vector.vectorPlus(vector, vector1);
         vector.info();
+
         vector.vectorMinus(vector, vector1);
         vector.info();
 
--- src/Vector.java
+++ src/Vector.java
@@ -16,8 +16,6 @@ public class Vector {
     }
 
 
-
-
     public void init(int n) {//создание автозаполнения
         Random random = new Random();
         arr = new int[n];
@@ -42,45 +40,49 @@ public class Vector {
         }
     }
 
-    public void opisProstranstva(){
+    public void opisProstranstva() {
         System.out.println("Это вектор " + arr.length + "-мерного пространства");
     }
 
 
-    public void vectorLength(){
+    public void vectorLength() {
         double length = 0;
-        for(int i = 0; i < arr.length; i++){
-            length+=Math.pow(arr[i],2);
+        for (int i = 0; i < arr.length; i++) {
+            length += Math.pow(arr[i], 2);
         }
         length = Math.sqrt(length);
         System.out.println("Длина вектора " + length);
     }
 
-    public Vector vectorPow(Vector vector1, Vector vector2){
-        if(vector1.arr.length != vector2.arr.length){
+    public Vector vectorPow(Vector vector1, Vector vector2) {
+        if (vector1.arr.length != vector2.arr.length) {
             System.out.println("пространства не совпадают");
             return vector1;
         }
-        for(int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             vector1.arr[i] = vector1.arr[i] * vector2.arr[i];
         }
         return vector1;
     }
-    public Vector vectorPlus(Vector vector1, Vector vector2){
-        if(vector1.arr.length != vector2.arr.length){
+
+
+    public Vector vectorPlus(Vector vector1, Vector vector2) {
+        if (vector1.arr.length != vector2.arr.length) {
             System.out.println("пространства не совпадают");
             return vector1;
         }
-        for(int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             vector1.arr[i] = vector1.arr[i] + vector2.arr[i];
         }
         return vector1;
     }
-    public Vector vectorMinus(Vector vector1, Vector vector2){
-        if(vector1.arr.length != vector2.arr.length){
+
+
+    public Vector vectorMinus(Vector vector1, Vector vector2) {
+        if (vector1.arr.length != vector2.arr.length) {
             return vector1;
         }
-        for(int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             vector1.arr[i] = vector1.arr[i] - vector2.arr[i];
         }
         return vector1;
